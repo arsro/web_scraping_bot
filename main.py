55a9b5f97b156cb4cd4944d6d0e485213c20650b
tomorrow anan <tomorrowanan@gmail.com>
2017-10-22 14:11:23 +0900

    [update] crawling article

--- main.py
+++ main.py
@@ -29,9 +29,6 @@ for categories in soup.find_all(class_='top-list__footer'):
 				mid_r = requests.get(mid_target_url)
 				mid_soap = BeautifulSoup(mid_r.text, 'lxml')
 				
-				# small
-				small_category = mid_soap.find(class_='ranking__title')
-				print('Small_category : ' + small_category.text)
 				
 				# article
 				for article_link in mid_soap.find_all(class_='list-article__link'):
@@ -41,9 +38,39 @@ for categories in soup.find_all(class_='top-list__footer'):
 					art_r = requests.get(article_link.get('href'))
 					art_soap = BeautifulSoup(art_r.text, 'lxml')
 						
-					art_title = art_soap.find(class_='single-article__title entry-title').text
-					print('article : ' + art_title)
-					
+					art_title = art_soap.find(class_='single-article__title entry-title')
+					print('article title: ' + art_title.text)
+						
+					# small
+					small_category = art_soap.find(class_='ranking__title')
+					print('Small_category : ' + small_category.text)
+
+
+					# supervisior
+					supervisior_position = art_soap.find(class_='supervisor-profile__position')
+					if supervisior_position is not None:
+						print('supervisior position : ' + supervisior_position.text)
+
+					supervisior_name = art_soap.find(class_='supervisor-profile__name')
+					if supervisior_name is not None:
+						print('supervisior name : ' + supervisior_name.text)
+
+					# related article link
+					for related_link in art_soap.find_all(class_='related__link'):
+						print('related link : ' + related_link.text)
+						# print('related link : ' + related_link.get('href'))
+
+					# reference
+					for reference_link in art_soap.find_all(class_='reference__link'):
+						print('reference link : ' + reference_link.text)
+						# print('reference link : ' + reference_link.get('href'))
+						
+						
+					# related keyword
+					related_kws = art_soap.find(class_="related-tags")
+					for related_kw in related_kws.select('ul > li'):
+						print('related kw : ' + related_kw.text)
+						
 					break
 				break
 print('end')
