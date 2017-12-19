2c358c1ff6072b1be23c1f7cd2ae17f0951d8c8d
tomorrow anan <tomorrowanan@gmail.com>
2017-12-19 13:24:38 +0900

    version3 bug fix

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ try:
 except IndexError:
 	target_category_url = ALL
 
-print(target_category_url)
-
 # topページをクローリング
 top_r = requests.get(TARGET_URL)
 top_soup = BeautifulSoup(top_r.text, 'lxml')
@@ -48,7 +46,6 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 		if done_crawling: break
 
 		# 大カテゴリ
-		print('Large_category : ' + re.sub(u'の関連カテゴリ', '', large_categories.find('h2').text))
 		large_category = re.sub(u'の関連カテゴリ', '', large_categories.find('h2').text)
 		
 		# 大カテゴリ内の中カテゴリを取得します。
@@ -58,7 +55,6 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 			for mid_category_link in mid_categories.select('ul > li > a'):
 				
 				# 中カテゴリ
-				print("Middle_category : " + mid_category_link.text)
 				middle_category = mid_category_link.text
 				
 				# 中カテゴリのリンク
@@ -73,8 +69,6 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 				while not(is_last):
 					page += 1
 					sleep(1)
-					print(mid_target_url + '/page/' + str(page))
-
 					#　中カテゴリページをクローリングします。
 					mid_r = requests.get(mid_target_url + '/page/' + str(page))
 					mid_soap = BeautifulSoup(mid_r.text, 'lxml')
@@ -91,7 +85,6 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 
 						
 						# 記事へのリンク
-						print('article link : ' + article_link.get('href'))
 						output_body.append(article_link.get('href'))
 						
 						sleep(1)
@@ -101,7 +94,6 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 
 						# 記事タイトル
 						art_title = art_soap.find(class_='single-article__title entry-title')
-						print('article title: ' + art_title.text)
 						output_body.append(art_title.text)
 						
 						# 記事の大カテゴリ
@@ -110,20 +102,17 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 						output_body.append(middle_category)
 						# 記事の小カテゴリ
 						small_category = art_soap.find(class_='ranking__title')
-						print('Small_category : ' + re.sub(u'人気ランキング', '', small_category.text))
 						output_body.append(re.sub(u'人気ランキング', '', small_category.text))
 
 						# 監修者がいれば、監修者の専門と名前を取得
 						supervisior_position = art_soap.find(class_='supervisor-profile__position')
 						if supervisior_position is not None:
-							print('supervisior position : ' + supervisior_position.text)
 							output_body.append(supervisior_position.text)
 						else:
 							output_body.append('')
 							
 						supervisior_name = art_soap.find(class_='supervisor-profile__name')
 						if supervisior_name is not None:
-							print('supervisior name : ' + supervisior_name.text)
 							output_body.append(supervisior_name.text)
 						else:
 							output_body.append('')
@@ -133,10 +122,8 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 						output_related_links = [''] * COLUMN_LIMIT
 						for related_link in art_soap.find_all(class_='related__link'):
 							output_related_links[i] = related_link.text
-							print('related link : ' + related_link.text)
 							i += 1
 							if i >= COLUMN_LIMIT:
-								print('OUT!')
 								break
 
 						for link in output_related_links:
@@ -148,11 +135,9 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 						output_reference_links = [''] * (COLUMN_LIMIT-5)
 						for reference_link in art_soap.find_all(class_='reference__link'):
 							output_reference_links[i] = reference_link.text
-							print('reference link : ' + reference_link.text)
 							# output_reference_links.append(reference_link.text)
 							i += 1
 							if i >= COLUMN_LIMIT-5:
-								print('OUT!')
 								break
 
 						for link in output_reference_links:
@@ -165,13 +150,9 @@ for categories in top_soup.find_all(class_='top-list__footer'):
 						related_kws = art_soap.find(class_="related-tags")
 						for related_kw in related_kws.select('ul > li > a'):
 							output_related_kws[i] = related_kw.text
-							print('related kw : ' + related_kw.text)
 							i += 1
 							if i >= COLUMN_LIMIT:
-								print('OUT!')
 								break
-
-						print(output_related_kws)
 
 						for kw in output_related_kws:
 							output_body.append(kw)
@@ -190,5 +171,3 @@ columns=[
 now = datetime.now().strftime('%y%m%d')
 
 pandas.DataFrame(output_bodies, columns=columns).to_csv("192abccom_"+now+".csv",encoding='utf-8')
-		
-print('end crawling!')
